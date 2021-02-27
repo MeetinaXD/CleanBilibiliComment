b78d404a9ac484aebeb3fdf32d83c9f243b065f0
MeetinaXD <capitaycity@gmail.com>
2021-02-27 18:29:13 +0800

    first release

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# CleanBilibiliComment
-Clean Bilibili comments
+# Clean Bilibili Comment
+最后更新: Febr 27, 2021.
+
+> 世人苦肉蛋奶久矣
+
+## 食用方法
+下载`clean-bilibili-comment.js`并导入到`Tampermonkey`中选择启用，或者直接在github打开`clean-bilibili-comment.js`，点击右上角的`RAW`，自己新建脚步直接全选覆盖即可。
+
+默认已经自带一条回形针的过滤示例，如果喜欢吃肉蛋奶，可以反向过滤（笑
+
+## 自定义规则 `对于0.2版本`
+在`第19行`找到`regExp`对象，按照第一条默认规则的格式复制编写。
+
+**默认规则如下**
+```js
+{ /* 回形针专用 */
+  author: 258150656,
+  title: /(回形针|PaperClip)+/,
+  filter: /(肉蛋奶|巴西雨林|黑子|买水|森林|人口|影响|黑名单|拉黑|雨林|原谅|敌军|毁掉|垄断|监控|打钱)+/,
+  init: "检测到回形针视频",
+  // set the flag as false to disable printing blocked comments
+  showBlocked: true
+},
+
+/* please put the global rule in the end */
+{
+  // use regular expression **obj** or author mid to match
+  author: null,
+  // use RegExp **obj** to match
+  title: /\w+/,
+  // use RegExp **obj** to match
+  filter: /(肉蛋奶|巴西雨林|黑子|买水|森林|人口|影响|黑名单|拉黑|雨林|原谅|敌军|毁掉|垄断|监控|打钱|水军)+/,
+  init: "全局应用生效"
+}
+```
+脚本按照匹配的方式运行，如果你在规则中指定了命中条件，那么这条规则只有在命中成功后开始工作。
+
+### 规则属性
+#### `author`
+**可选属性**
+`string` `number` `正则对象[object RegExp]`
+
+**命中方式**
+当指定类型为`number`时，视为`author mid`，会进行全等判断
+
+当指定类型为`string`时，视为`author name`，会进行模糊搜索
+
+当指定类型为`[object RegExp]`时，将调用对象的`test`方法对作者名称进行正则匹配
+
+> 如不需要，可设置为`null`
+
+#### `title`
+**可选属性**
+`string` `正则对象[object RegExp]`
+
+**命中方式**
+当指定类型为`string`时，视为`video title`，会进行模糊搜索
+
+当指定类型为`[object RegExp]`时，将调用对象的`test`方法对视频标题进行正则匹配
+
+> 如不需要，可设置为`null`
+
+#### `filter`
+可选属性
+`正则对象[object RegExp]`
+
+提供一个正则表达式对象，过滤时将使用表达式的`test()`方法进行过滤
+当`test()`返回`true`时，评论将被屏蔽
+
+#### `init`
+可选属性
+`string` `function`
+
+传入一个字符串或函数，将在初始化完成后输出或调用
+
+#### `showBlocked`
+可选属性
+`boolean`
+
+指明是否需要打印被屏蔽的评论，如果需要调试过滤条件，建议开启
+
+### 全局规则
+全局规则见默认规则最后一条，将命中条件设置一定可以命中的条件，如`/\w*/`。
+
+**一定要放在最后**！否则在它后面的条件都会失效。
+
+## 最后
+如果愿意分享你的规则，可以在issue留言。
+
+愿天堂没有评论区。
---
+++ clean-bilibili-comment.js
@@ -0,0 +1,149 @@
+// ==UserScript==
+// @name         Clean Bilibili Comment
+// @namespace    https://meetinaxd.ltiex.com
+// @version      0.2
+// @description  To be or not to be, that's a question.
+// @author       MeetinaXD
+// @match        https://www.bilibili.com/video/*
+// @grant        none
+// ==/UserScript==
+
+// window.vd -> bilibili video info.
+// :: title -> video title
+// :: owner -> video author info.
+
+// window.bbComment -> related to comment and reply
+
+(function () {
+  // put your rules here...
+  const regExp = [
+    { /* 回形针专用 */
+      author: 258150656,
+      title: /(回形针|PaperClip)+/,
+      filter: /(肉蛋奶|巴西雨林|黑子|买水|森林|人口|影响|黑名单|拉黑|雨林|原谅|敌军|毁掉|垄断|监控|打钱)+/,
+      init: "检测到回形针视频",
+      // set the flag as false to disable printing blocked comments
+      showBlocked: true
+    },
+
+    /* please put the global rule in the end */
+    {
+      // use regular expression **obj** or author mid to match
+      author: null,
+      // use RegExp **obj** to match
+      title: /\w+/,
+      // use RegExp **obj** to match
+      filter: /(肉蛋奶|巴西雨林|黑子|买水|森林|人口|影响|黑名单|拉黑|雨林|原谅|敌军|毁掉|垄断|监控|打钱|水军)+/,
+      init: "全局应用生效"
+    }
+  ]
+
+  let currentExp = null
+  function init() {
+    const commentBlock = "<span style='background: #eaeaea; color:#3e3e3e; font-size:14px'>This comment has been blocked</span>"
+    const urlBlock = "<span style='background: #eaeaea; color:red; font-size:14px'>This url has been blocked</span>"
+
+    // Comment and reply rendering method
+    const fn1 = window.bbComment.prototype._createMsgContent
+    const fn2 = window.bbComment.prototype._createSubMsgContent
+    const fn3 = window.bbComment.prototype._resolveJump
+
+    // Once the script handle the original render methods, the following objects will not be inited correctly.
+    window.bbComment.prototype.jumpReportIndex = 0
+    window.bbComment.prototype.jumpReport = {}
+
+    window.bbComment.prototype._createMsgContent = function (e) {
+      return handler(e, fn1)
+    }
+    window.bbComment.prototype._createSubMsgContent = function (e) {
+      return handler(e, fn2)
+    }
+    window.bbComment.prototype._resolveJump = function (e, t) {
+      return handler(e, fn3, t)
+    }
+
+    const handler = function (e, fn, extra = null) {
+      const exp = currentExp.filter
+      if (extra) {
+        let replace = ""
+        let k
+        for (k in extra){
+          if (exp.test(extra[k].title)){
+            extra[k] = {}
+            replace += urlBlock
+          }
+        }
+        return replace + fn.call(window.bbComment.prototype, e, extra)
+      }
+
+      let domText = commentBlock
+      let text = e.content.message
+      if (!exp.test(text)) {
+        domText = fn.call(window.bbComment.prototype, e)
+      } else if(currentExp.showBlocked) {
+        console.log('\t%c已过滤 >>> ', "color: orange",  text)
+      }
+      return domText
+    }
+  }
+
+  const intervalId = setInterval(function () {
+    // init exp
+    if (window.vd && !currentExp) {
+      const vd = window.vd
+      const authorName = vd.owner.name
+      const authorMid = vd.owner.mid
+      const title = vd.title
+      function setExp(o) {
+        if (!o && typeof o.filter !== "[object RegExp]"){
+          console.error("[Clean Bilbili Comment] Your filter is illegal")
+        }
+        currentExp = o
+      }
+      for (let i = 0; i < regExp.length; i++) {
+        const exp = regExp[i];
+        if (exp.author) {
+          const type = typeof exp.author
+          if (type === "string" && authorName.indexOf(exp.author) === -1){
+            continue
+          }
+          if (type === "number" && authorMid !== exp.author){
+            continue
+          }
+          if (type === "[object RegExp]" && !exp.author.test(authorName)){
+            continue
+          }
+          setExp(exp)
+          break
+        }
+        if(exp.title){
+          const type = typeof exp.title
+          if (type === "string" && title.indexOf(exp.title) === -1){
+            continue
+          }
+          if (type === "[object RegExp]" && !exp.title.test(title)){
+            continue
+          }
+          setExp(exp)
+          break
+        }
+      }
+      // no match video, cancel.
+      if (!currentExp){
+        console.warn("[Clean Bilbili Comment] 无匹配条件")
+        clearInterval(intervalId)
+      }
+    }
+
+    if (currentExp && typeof $ !== 'undefined' && $('.comment').length !== 0 && !!window.bbComment && !!window.vd) {
+      clearInterval(intervalId)
+      init()
+      if (typeof currentExp.init === 'function'){
+        currentExp.init()
+      } else if (typeof currentExp.init === 'string') {
+        console.warn(currentExp.init)
+      }
+      console.warn("[Clean Bilbili Comment] Bilibili 评论区净化注入成功")
+    }
+  }, 100)
+})();
